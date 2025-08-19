63a91a2fe4b65423ee602b5a6caae7511157ba7b
Joren D. Bagadiong <j.bagadiong@psp.team>
2025-08-19 20:12:48 +0800

    fix social links

--- components/Social.tsx
+++ components/Social.tsx
@@ -18,20 +18,23 @@ type Props = {
 }
 
 const SOCIALS: SocialItem[] = [
-  { icon: FaLinkedinIn, path: "" },
+  {
+    icon: FaLinkedinIn,
+    path: "https://www.linkedin.com/in/joren-bagadiong-8943a3213",
+  },
   { icon: FaTelegramPlane, path: "https://t.me/jorenbagadiong" },
   {
     icon: FaFacebookMessenger,
     path: "https://m.me/jorenbagadiong",
   },
-  { icon: FaFacebookF, path: "/https://www.facebook.com/jorenbagadiong" },
+  { icon: FaFacebookF, path: "https://www.facebook.com/jorenbagadiong" },
 ]
 
 const Social = ({ containerStyles = "", iconStyles = "" }: Props) => {
   return (
     <div className={containerStyles}>
       {SOCIALS.map(({ path, icon: Icon }, index) => (
-        <Link key={index} href={path} className={iconStyles}>
+        <Link key={index} href={path} className={iconStyles} target="_blank">
           <Icon />
         </Link>
       ))}
